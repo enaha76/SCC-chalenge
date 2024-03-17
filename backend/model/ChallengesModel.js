1f8dd00228ce27132d6149bbe4f584e72332f38a
elkherchi <22025@supnum.mr>
2024-03-17 17:20:16 +0000

    envoyer

--- backend/model/ChallengesModel.js
+++ backend/model/ChallengesModel.js
@@ -1,9 +1,5 @@
 const { Sequelize } = require("sequelize");
-<<<<<<< HEAD
 const conn = require("../config/connect.js");
-=======
-const conn = require("../config/connect");
->>>>>>> fd9dfb8df3b1103c19c63dd378e7af047bbf0c4a
 const Users = require("./UserModels.js");
 const { DataTypes } = Sequelize;
 
@@ -18,7 +14,6 @@ const Challenges = conn.define(
         len: [3, 100],
       },
     },
-<<<<<<< HEAD
     description :{
         type: DataTypes.STRING,
         allowNull: false,
@@ -53,44 +48,3 @@ conn.sync()
         console.error("Erreur lors de la création de la table 'Challenges':", err);
     });
 module.exports = Challenges;
-=======
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    deadline: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    organizer_user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  },
-  {
-    freezeTableName: true,
-  }
-);
-Users.hasMany(Challenges);
-Challenges.belongsTo(Users, { foreignKey: "organizer_user_id " });
-conn
-  .sync()
-  .then(() => {
-    console.log(
-      "La table 'users' a été créée avec succès dans la base de données."
-    );
-  })
-  .catch((err) => {
-    console.error("Erreur lors de la création de la table 'users':", err);
-  });
-module.exports = Challenges;
->>>>>>> fd9dfb8df3b1103c19c63dd378e7af047bbf0c4a
